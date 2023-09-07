16c80aeaf0218f24b62634c22a1e30e38a0dd126
Viktor Ivannikov <iceivannikov@gmail.com>
2023-09-07 15:15:08 +0300

    init + add .gitignore + add Task 1

---
+++ VariablesTheme.java
@@ -0,0 +1,28 @@
+public class VariablesTheme {
+    
+    public static void main(String[] args) {
+        System.out.println("Task 1: Displaying computer characteristics");
+        String model = "MacBook Pro";
+        byte storage = 1;
+        short updateFrequency = 120;
+        int memory = 32;
+        long camera = 1080;
+        float wifi = 802.11f;
+        double thickness = 1.68;
+        double length = 35.57;
+        double width = 24.81;
+        double weight = 2.17;
+        char meter = 'm';
+        boolean bluetooth = true;
+        System.out.println("Model: " + model);
+        System.out.println("Update frequency " + updateFrequency);
+        System.out.println("Storage: " + 1 + " Gb");
+        System.out.println("Memory: " + memory + " Gb");
+        System.out.println("Camera FaceTime HD " + camera + "p");
+        System.out.println("Connecting to networks Wi‑Fi 6 " + wifi + " ax");
+        System.out.println("Cable USB‑C/MagSafe 3 (2 " + meter + ")");
+        System.out.println("Bluetooth 5.0 " + bluetooth);
+        System.out.println();
+
+    }
+}
