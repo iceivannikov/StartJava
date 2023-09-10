da6ee62fd758784e29b41ca175864ccb7155eb88
Viktor Ivannikov <iceivannikov@gmail.com>
2023-09-10 23:22:49 +0300

    add Task 1 Translating pseudocode into Java

---
+++ Lesson_1/IfElseStatementTheme.java
@@ -0,0 +1,13 @@
+public class IfElseStatementTheme {
+
+    public static void main(String[] args) {
+        System.out.println("\n==Task 1: Translating pseudocode into Java==");
+        boolean isMale = true;
+        if (!isMale) {
+            System.out.println("female");
+        } else {
+            System.out.println("male\n");
+        }
+
+    }
+}
