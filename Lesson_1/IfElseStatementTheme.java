587411d5a1f9ac8ba12b4d0aa7ec62b2a050883b
Viktor Ivannikov <iceivannikov@gmail.com>
2023-09-10 23:53:50 +0300

    Added all subtasks to the task

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -1,13 +1,36 @@
 public class IfElseStatementTheme {
 
     public static void main(String[] args) {
-        System.out.println("\n==Task 1: Translating pseudocode into Java==");
+        System.out.println("\n==Task 1: Translating pseudocode into Java==\n");
         boolean isMale = true;
         if (!isMale) {
-            System.out.println("female");
+            System.out.println("female\n");
         } else {
             System.out.println("male\n");
         }
 
+        int age = 20;
+        if (age > 18) {
+            System.out.println("age larger 18\n");
+        } else {
+            System.out.println("age less 18\n");
+        }
+
+        double height = 2.3;
+        if (height < 1.8) {
+            System.out.println("height larger 1.8\n");
+        } else {
+            System.out.println("height less 1.8\n");
+        }
+
+        String name = "Maksim";
+        char firstLetterOfName = name.charAt(0);
+        if (firstLetterOfName == 'M') {
+            System.out.println("First letter of name - M\n");
+        } else if (firstLetterOfName == 'I') {
+            System.out.println("First letter of name - I\n");
+        } else {
+            System.out.println("No suitable options\n");
+        }
     }
 }
