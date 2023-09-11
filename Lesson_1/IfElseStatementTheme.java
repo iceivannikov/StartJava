09d0bac126a0c9950d29e4d2a66490172169f410
Viktor Ivannikov <iceivannikov@gmail.com>
2023-09-11 18:23:54 +0300

    add Task 4 Finding the same digits in numbers

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -4,36 +4,36 @@ public class IfElseStatementTheme {
         System.out.println("\n==Task 1: Translating pseudocode into Java==\n");
         boolean isMale = true;
         if (!isMale) {
-            System.out.println("female\n");
+            System.out.println("female");
         } else {
-            System.out.println("male\n");
+            System.out.println("male");
         }
 
         int age = 20;
         if (age > 18) {
-            System.out.println("age larger 18\n");
+            System.out.println("age larger 18");
         } else {
-            System.out.println("age less 18\n");
+            System.out.println("age less 18");
         }
 
         double height = 2.3;
         if (height < 1.8) {
-            System.out.println("height larger 1.8\n");
+            System.out.println("height larger 1.8");
         } else {
-            System.out.println("height less 1.8\n");
+            System.out.println("height less 1.8");
         }
 
         String name = "Maksim";
         char firstLetterOfName = name.charAt(0);
         if (firstLetterOfName == 'M') {
-            System.out.println("First letter of name - M\n");
+            System.out.println("First letter of name - M");
         } else if (firstLetterOfName == 'I') {
-            System.out.println("First letter of name - I\n");
+            System.out.println("First letter of name - I");
         } else {
-            System.out.println("No suitable options\n");
+            System.out.println("No suitable options");
         }
 
-        System.out.println("==Task 2: Search for more number==\n");
+        System.out.println("\n==Task 2: Search for more number==\n");
         int a = 5, b = 10;
         if (a < b) {
             System.out.println("a less b");
@@ -47,7 +47,8 @@ public class IfElseStatementTheme {
         int x = 0;
         if (x == 0) {
             System.out.println("The number is zero");
-        } else if (x < 0 && x % 2 ==0) {
+        }
+        if (x < 0 && x % 2 ==0) {
             System.out.println("Number " + x + " is negative and even");
         } else if (x < 0 && x % 2 !=0) {
             System.out.println("Number " + x + " is negative and odd");
@@ -56,5 +57,50 @@ public class IfElseStatementTheme {
         } else {
             System.out.println("Number " + x + " is positive and odd");
         }
+
+        System.out.println("\n==Task 4: Finding the same digits in numbers==\n");
+        int c = 153, d = 443;
+        int hundredsC = c / 100;
+        int hundredsD = d / 100;
+        int tensC = c / 10 % 10;
+        int tensD = d / 10 % 10;
+        int onesC = c % 10;
+        int onesD = d % 10;
+        int hundreds = 3;
+        int tens = 2;
+        int ones = 1;
+        if (hundredsC != hundredsD && tensC != tensD && onesC != onesD) {
+            System.out.println("The numbers in the digits are different");
+        }
+        if (hundredsC == hundredsD && tensC == tensD && onesC == onesD) {
+            System.out.println("Numbers " + c + " " + d + " have the same digits " + hundredsC  + 
+                " and " + hundredsD + ", place number " + hundreds + ", " + tensC + " and " + tensD
+                + ", place number " + tens + ", " + onesC + " and " + onesD + ", place number " +
+                ones);
+        } else if (hundredsC == hundredsD && tensC == tensD) {
+            System.out.println("Numbers " + c + " " + d + " have the same digits " + hundredsC + 
+                " and " + hundredsD + ", place number " + hundreds + ", " + tensC + " and " + tensD
+                + ", place number " + tens);
+        } else if (tensC == tensD && onesC == onesD) {
+            System.out.println("Numbers " + c + " " + d + " have the same digits " + tensC + 
+                " and " + tensD + ", place number " + tens + ", " + onesC + " and " + onesD + 
+                ", place number " + ones);
+        } else if (hundredsC == hundredsD && onesC == onesD) {
+            System.out.println("Numbers " + c + " " + d + " have the same digits " + hundredsC  + 
+                " and " + hundredsD + ", place number " + hundreds + ", " + onesC + " and " + 
+                onesD + ", place number " + ones);
+        } else if (hundredsC == hundredsD) {
+            System.out.println("Numbers " + c + " " + d + " have the same digits " + hundredsC + 
+                " and " + hundredsD + ", place number " + hundreds);
+        } else if (tensC == tensD) {
+            System.out.println("Numbers " + c + " " + d + " have the same digits " + tensC + 
+                " and " + tensD + ", place number " + tens);
+        } else if (onesC == onesD) {
+            System.out.println("Numbers " + c + " " + d + " have the same digits " + onesC + 
+                " and " + onesD + ", place number " + ones);
+        }
+
+        System.out.println("\n==Task 5: Determining a symbol by its code==\n");
+        
     }
 }
