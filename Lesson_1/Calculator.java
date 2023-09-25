1cc3ca605eef43b49ee58c6b1a8b2aed562267bc
Viktor Ivannikov <iceivannikov@gmail.com>
2023-09-25 12:04:28 +0300

    Task Calculator Error correction #1

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,34 +2,35 @@ public class Calculator {
 
     public static void main(String[] args) {
         System.out.println("==Task Calculator==");
-        int value1 = 2;
-        int value2 = 0;
+        int a = 4;
+        int b = 0;
         int result = 0;
-        char sign = '%';
+        char sign = '/';
         if (sign == '+') {
-            result = value1 + value2;
+            result = a + b;
         } else if (sign == '-') {
-            result = value1 - value2;
+            result = a - b;
         } else if (sign == '*') {
-            result = value1 * value2;
+            result = a * b;
         } else if (sign == '^') {
-            result = value1;
-            for (int i = 1; i < value2; i++) {
-                result *= value1;
+            for (int i = 1; i < b; i++) {
+                result = a * a;
             }
         } else if (sign == '%' || sign == '/') {
-            if (value2 == 0) {
+            if (b == 0) {
                 System.out.println("You can't divide by zero");
             } else if (sign == '%') {
-                result = value1 % value2;
+                result = a % b;
             } else {
-                result = value1 / value2;
+                result = a / b;
             }
         }
-        if (value2 == 0 && sign == '%' || value2 == 0 && sign == '/') {
-            System.out.println("Value two cannot be zero");
+        if (sign == '%' || sign == '/') {
+            if (b == 0) {
+                System.out.println("Value b cannot be zero");
+            }
         } else {
-            System.out.println(value1 + " " + sign + " " + value2 + " = " + result);
+            System.out.println(a + " " + sign + " " + b + " = " + result);
         }
     }
 }
