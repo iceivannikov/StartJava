beea7c3c97f7fcbee01cee7afe394e83338f1714
Viktor Ivannikov <iceivannikov@gmail.com>
2023-09-25 12:43:13 +0300

    Task Calculator Error correction #2

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,10 +2,10 @@ public class Calculator {
 
     public static void main(String[] args) {
         System.out.println("==Task Calculator==");
-        int a = 4;
-        int b = 0;
+        int a = 3;
+        int b = 3;
         int result = 0;
-        char sign = '/';
+        char sign = '%';
         if (sign == '+') {
             result = a + b;
         } else if (sign == '-') {
@@ -13,24 +13,20 @@ public class Calculator {
         } else if (sign == '*') {
             result = a * b;
         } else if (sign == '^') {
+            result = a;
             for (int i = 1; i < b; i++) {
-                result = a * a;
+                result *= a;
             }
         } else if (sign == '%' || sign == '/') {
             if (b == 0) {
                 System.out.println("You can't divide by zero");
+                return;
             } else if (sign == '%') {
                 result = a % b;
             } else {
                 result = a / b;
             }
         }
-        if (sign == '%' || sign == '/') {
-            if (b == 0) {
-                System.out.println("Value b cannot be zero");
-            }
-        } else {
-            System.out.println(a + " " + sign + " " + b + " = " + result);
-        }
+        System.out.println(a + " " + sign + " " + b + " = " + result);
     }
 }
