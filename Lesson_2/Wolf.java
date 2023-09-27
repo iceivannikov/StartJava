23c0a34f079721574ac826c58aac561fc12916bf
Viktor Ivannikov <iceivannikov@gmail.com>
2023-09-27 16:33:33 +0300

    add class Wolf

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+    String gender;
+    String name;
+    double weight;
+    int age;
+    String color;
+
+    void move() {
+        System.out.println("coming");
+    }
+
+    void sit() {
+        System.out.println("is sitting");
+    }
+
+    void run() {
+        System.out.println("runs");
+    }
+
+    void howl() {
+        System.out.println("howls");
+    }
+
+    void hunt() {
+        System.out.println("I'm hunting");
+    }
+}
