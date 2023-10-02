555e25364f0312d60893af0f6278368ae9bd1bb3
Viktor Ivannikov <iceivannikov@gmail.com>
2023-10-02 15:37:23 +0300

    Error correction #1

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GuessNumber {
 
     private Player player1;
@@ -20,7 +22,23 @@ public class GuessNumber {
         return maxNumber;
     }
 
-    public void checkNumber(int number, int answer, Player player) {
+    public void start(int number, int answer) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("The computer has guessed a number, the game begins");
+        while(number != answer) {
+            System.out.print("Player " + player1.getName() + " is your number: ");
+            answer = sc.nextInt();
+            checkNumber(number, answer, player1);
+            if (number == answer) {
+                break;
+            }
+            System.out.print("Player " + player2.getName() + " is your number: ");
+            answer = sc.nextInt();
+            checkNumber(number, answer, player2);
+        }
+    }
+
+    private void checkNumber(int number, int answer, Player player) {
         if (answer == number) {
             System.out.println("Player " + player.getName() + " win!!!");
             return;
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -11,34 +11,20 @@ public class GuessNumberTest {
         while(!option.equals("no")) {
             System.out.println("\nTo start the game you need to specify the names of the players");
             System.out.print("Enter the name of the first player: ");
-            String namePlayer1 = sc.nextLine();
+            String namePlayer1 = sc.next();
             System.out.print("Enter the name of the second player: ");
-            String namePlayer2 = sc.nextLine();
+            String namePlayer2 = sc.next();
             Player player1 = new Player(namePlayer1);
             Player player2 = new Player(namePlayer2);
             GuessNumber guessNumber = new GuessNumber(player1, player2);
             int minNumber = guessNumber.getMinNumber();
             int maxNumber = guessNumber.getMaxNumber();
             int number = random.nextInt(minNumber, maxNumber);
-            System.out.println("The computer has guessed a number, the game begins");
-            while(number != answer) {
-                System.out.print("Player " + player1.getName() + " is your number: ");
-                answer = sc.nextInt();
-                guessNumber.checkNumber(number, answer, player1);
-                if (number == answer) {
-                    break;
-                }
-                System.out.print("Player " + player2.getName() + " is your number: ");
-                answer = sc.nextInt();
-                guessNumber.checkNumber(number, answer, player2);
-            }
-            while(!option.equals("yes")) {
+            guessNumber.start(number, answer);
+            do {
                 System.out.print("Do you want to continue playing? [yes/no]: ");
                 option = sc.next();
-                if (option.equals("no")) {
-                    break;
-                }
-            }
+            } while(!option.equals("yes") && !option.equals("no"));
         }
         sc.close();
     }
