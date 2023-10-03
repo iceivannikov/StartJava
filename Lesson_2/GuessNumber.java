a4f4de5d59224c893686937994872b0eb2a5de0a
Viktor Ivannikov <iceivannikov@gmail.com>
2023-10-03 19:36:32 +0300

    Error correction #4

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -6,9 +6,10 @@ public class GuessNumber {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 101;
 
+    private final Scanner sc = new Scanner(System.in);
+
     private Player player1;
     private Player player2;
-    private final Scanner sc = new Scanner(System.in);
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -17,32 +18,34 @@ public class GuessNumber {
 
     public void start() {
         Random random = new Random();
-        int number = random.nextInt(MIN_NUMBER, MAX_NUMBER);
-        int answer = 0;
+        int guessNumber = random.nextInt(MIN_NUMBER, MAX_NUMBER);
+        int answerNumber = 0;
         System.out.println("The computer has guessed a number, the game begins");
-        while(number != answer) {
-            answer = getAnswer(player1);
-            if (checkNumber(number, answer, player1)) {
+        while(true) {
+            answerNumber = inputAnswer(player1);
+            if (isGuessed(guessNumber, answerNumber, player1)) {
                 break;
             }
-            answer = getAnswer(player2);
-            checkNumber(number, answer, player2);
+            answerNumber = inputAnswer(player2);
+            if (isGuessed(guessNumber, answerNumber, player2)) {
+                break;
+            }
         }
     }
 
-    private int getAnswer(Player player) {
+    private int inputAnswer(Player player) {
         System.out.printf("Player %s is your number: ", player.getName());
         return sc.nextInt();
     }
 
-    private boolean checkNumber(int number, int answer, Player player) {
-        boolean isEquals = answer == number;
+    private boolean isGuessed(int guessNumber, int answerNumber, Player player) {
+        boolean isEquals = answerNumber == guessNumber;
         if (isEquals) {
             System.out.printf("Player %s win!!!\n", player.getName());
             return isEquals;
         }
         System.out.printf("The number %d is %s than what the computer guessed\n", 
-                answer, (answer > number) ? "greater" : "less");
+                answerNumber, (answerNumber > guessNumber) ? "greater" : "less");
         return isEquals;
     }
 }
