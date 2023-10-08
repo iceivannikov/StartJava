93e842626502f12c1800dd31993c97c9febb350e
Viktor Ivannikov <iceivannikov@gmail.com>
2023-10-08 19:03:12 +0300

    refactor method sort

--- src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
@@ -117,8 +117,8 @@ public class ArraysTheme {
     }
 
     private static void sort(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers.length - 1; j++) {
+        for (int i = numbers.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 if (numbers[j] > numbers[j + 1]) {
                     int tmp = numbers[j + 1];
                     numbers[j + 1] = numbers[j];
