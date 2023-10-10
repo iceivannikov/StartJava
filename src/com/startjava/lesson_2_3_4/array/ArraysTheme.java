df46e93b2c0d3c2b500c9d2aaf0ffb91eae7ad1f
Viktor Ivannikov <iceivannikov@gmail.com>
2023-10-10 20:49:00 +0300

    error correction #2

--- src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
@@ -15,12 +15,11 @@ public class ArraysTheme {
         System.out.println("\n== 1 Reversing array values ==");
         int[] numbers = {1, 7, 4, 5, 2, 6, 3};
         System.out.println("Before reverse: " + Arrays.toString(numbers));
-        int length = numbers.length - 1;
+        int length = numbers.length;
         for (int i = 0; i < length; i++) {
-            int tmp = numbers[length];
+            int tmp = numbers[--length];
             numbers[length] = numbers[i];
             numbers[i] = tmp;
-            length--;
         }
         System.out.println("After reverse: " + Arrays.toString(numbers));
     }
@@ -28,13 +27,13 @@ public class ArraysTheme {
     private static void productArrayElements() {
         System.out.println("\n== 2 Product of array elements ==");
         int length = 10;
-        int[] numbers = new int[length];
+        int[] multipliers = new int[length];
         for (int i = 0; i < length; i++) {
-            numbers[i] = i;
+            multipliers[i] = i;
         }
         int result = 1;
         for (int i = result; i < length - 1; i++) {
-            result *= numbers[i];
+            result *= multipliers[i];
             System.out.print(i + (i < length - 2 ? " * " : " = "));
         }
         System.out.print(result);
@@ -65,13 +64,12 @@ public class ArraysTheme {
     private static void displayingAlphabet() {
         System.out.println("\n\n== 4 Displaying the alphabet using a ladder ==");
         char[] alphabet = new char[26];
-        char letter = 'A';
         for (int i = 0; i < alphabet.length; i++) {
-            alphabet[i] = letter++;
+            alphabet[i] = (char) ('A' + i);
         }
         String result = "";
-        for (int j = alphabet.length - 1; j >= 0; j--) {
-            result += alphabet[j];
+        for (int i = alphabet.length - 1; i >= 0; i--) {
+            result += alphabet[i];
             System.out.println(result);
         }
     }
@@ -80,7 +78,8 @@ public class ArraysTheme {
         System.out.println("\n== 5 Filling an array with unique numbers ==");
         int[] uniqueNumbers = new int[30];
         int count = 0;
-        while (count < uniqueNumbers.length) {
+        int length = uniqueNumbers.length;
+        while (count < length) {
             int randomNumber = (int) (Math.random() * 40) + 60;
             boolean isDuplicate = false;
             for (int i = 0; i < count; i++) {
@@ -95,12 +94,7 @@ public class ArraysTheme {
             }
         }
         sort(uniqueNumbers);
-        for (int i = 0; i < uniqueNumbers.length; i++) {
-            System.out.print(uniqueNumbers[i] + (i < uniqueNumbers.length - 1 ? ", " : "."));
-            if ((i + 1) % 10 == 0) {
-                System.out.println();
-            }
-        }
+        print(length, uniqueNumbers);
     }
 
     private static void print(double[] array) {
@@ -112,14 +106,23 @@ public class ArraysTheme {
         }
     }
 
-    private static void sort(int[] numbers) {
-        for (int i = numbers.length - 1; i > 0; i--) {
+    private static void sort(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    int tmp = numbers[j + 1];
-                    numbers[j + 1] = numbers[j];
-                    numbers[j] = tmp;
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
                 }
+            }
+        }
+    }
+
+    private static void print(int length, int[] array) {
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] + " ");
+            if ((i + 1) % 10 == 0) {
+                System.out.println();
             }
         }
     }
