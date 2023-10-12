7faa30ad9eeaf96bfab2a13e5eaa62ebdaa0f7f3
Viktor Ivannikov <iceivannikov@gmail.com>
2023-10-12 15:31:06 +0300

    error correction #2 Lesson 4

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -12,51 +12,29 @@ public class Calculator {
     private static final String EXCEPTION_MESSAGE = "Exception!!! You can't divide by zero";
     private static final String INCORRECT_OPERATION_SIGN = "Error: sign [ %s ] is not supported\n";
 
-    private int a;
-    private String sign;
     private int b;
 
-    public Calculator() {
-    }
-
-    public int getA() {
-        return a;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public double calculate(String mathOperation) {
-        String[] partsExpression = mathOperation.split(DELIMITER_REGEX);
-        a = Integer.parseInt(partsExpression[0]);
-        sign = partsExpression[1];
+    public double calculate(String expression) {
+        String[] partsExpression = expression.split(DELIMITER_REGEX);
+        int a = Integer.parseInt(partsExpression[0]);
+        String sign = partsExpression[1];
         b = Integer.parseInt(partsExpression[2]);
-        try {
-            return switch (sign) {
-                case PLUS -> a + b;
-                case MINUS -> a - b;
-                case MULTIPLY -> a * b;
-                case RAISE_TO_POWER -> Math.pow(a, b);
-                case DIVIDE -> {
-                    if (b == 0) {
-                        System.out.println(EXCEPTION_MESSAGE);
-                        yield Double.MIN_VALUE;
-                    } else {
-                        yield (double) a / b;
-                    }
-                }
-                case REMAINDER_WITH_DIVISION -> a % b;
-                default -> {
-                    System.out.printf(INCORRECT_OPERATION_SIGN, sign);
-                    yield Double.MIN_VALUE;
-                }
-            };
-        } catch (ArithmeticException ex) {
+        return switch (sign) {
+            case PLUS -> a + b;
+            case MINUS -> a - b;
+            case MULTIPLY -> a * b;
+            case RAISE_TO_POWER -> Math.pow(a, b);
+            case DIVIDE -> checkByZero() == Double.MIN_VALUE ? Double.MIN_VALUE : (double) a / b;
+            case REMAINDER_WITH_DIVISION -> checkByZero() == Double.MIN_VALUE ? Double.MIN_VALUE : a % b;
+            default -> {
+                System.out.printf(INCORRECT_OPERATION_SIGN, sign);
+                yield Double.MIN_VALUE;
+            }
+        };
+    }
+
+    private double checkByZero() {
+        if (b == 0) {
             System.out.println(EXCEPTION_MESSAGE);
         }
         return Double.MIN_VALUE;
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -4,32 +4,31 @@ import java.util.Scanner;
 
 public class CalculatorTest {
 
-    private static Scanner sc;
+    private static final Scanner sc = new Scanner(System.in);
     public static final String CONTINUE_OR_END = "Do you want to continue calculating? [yes/no]: ";
     public static final String ENTER_EXPRESSION = "\nEnter a mathematical expression: ";
     public static final String YES = "yes";
     public static final String NO = "no";
 
     public static void main(String[] args) {
-        sc = new Scanner(System.in);
-        while (true) {
+        do {
             System.out.print(ENTER_EXPRESSION);
             String expression = sc.nextLine();
             Calculator calc = new Calculator();
             double result = calc.calculate(expression);
-            printResult(result, calc);
-            if (!continueOrEnd()) {
-                return;
-            }
+            printResult(result, expression);
+        } while (isNext());
+    }
+
+    private static void printResult(double result, String expression) {
+        if (result == Double.MIN_VALUE) {
+            System.out.println();
+        } else {
+            System.out.printf("%s = " + (result % 1 == 0 ? "%.0f\n" : "%.3f\n"), expression, result);
         }
     }
 
-    private static void printResult(double result, Calculator calc) {
-        System.out.printf((result == Double.MIN_VALUE) ? "" : "%d %s %d = " +
-                (result % 1 == 0 ? "%.0f\n" : "%.3f\n"), calc.getA(), calc.getSign(), calc.getB(), result);
-    }
-
-    private static boolean continueOrEnd() {
+    private static boolean isNext() {
         String answer;
         do {
             System.out.print(CONTINUE_OR_END);
