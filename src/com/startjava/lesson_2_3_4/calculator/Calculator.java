53e501367f7f6c4e77bf1c51f7c3b0c22923a04a
Viktor Ivannikov <iceivannikov@gmail.com>
2023-10-13 12:57:14 +0300

    error correction #3 Lesson 4

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -9,34 +9,30 @@ public class Calculator {
     private static final String DIVIDE = "/";
     private static final String REMAINDER_WITH_DIVISION = "%";
     private static final String DELIMITER_REGEX = " ";
-    private static final String EXCEPTION_MESSAGE = "Exception!!! You can't divide by zero";
-    private static final String INCORRECT_OPERATION_SIGN = "Error: sign [ %s ] is not supported\n";
-
-    private int b;
+    private static final String DIVISION_BY_ZERO_EXCEPTION_MSG = "Exception!!! You can't divide by zero";
+    private static final String NO_SIGN_MSG = "Error: sign [ %s ] is not supported\n";
 
     public double calculate(String expression) {
         String[] partsExpression = expression.split(DELIMITER_REGEX);
         int a = Integer.parseInt(partsExpression[0]);
         String sign = partsExpression[1];
-        b = Integer.parseInt(partsExpression[2]);
+        int b = Integer.parseInt(partsExpression[2]);
         return switch (sign) {
             case PLUS -> a + b;
             case MINUS -> a - b;
             case MULTIPLY -> a * b;
             case RAISE_TO_POWER -> Math.pow(a, b);
-            case DIVIDE -> checkByZero() == Double.MIN_VALUE ? Double.MIN_VALUE : (double) a / b;
-            case REMAINDER_WITH_DIVISION -> checkByZero() == Double.MIN_VALUE ? Double.MIN_VALUE : a % b;
+            case DIVIDE -> b == 0 ? getDoubleMinValueAndPrintExMsg() : (double) a / b;
+            case REMAINDER_WITH_DIVISION -> b == 0 ? getDoubleMinValueAndPrintExMsg() : a % b;
             default -> {
-                System.out.printf(INCORRECT_OPERATION_SIGN, sign);
+                System.out.printf(NO_SIGN_MSG, sign);
                 yield Double.MIN_VALUE;
             }
         };
     }
 
-    private double checkByZero() {
-        if (b == 0) {
-            System.out.println(EXCEPTION_MESSAGE);
-        }
+    private double getDoubleMinValueAndPrintExMsg() {
+        System.out.println(DIVISION_BY_ZERO_EXCEPTION_MSG);
         return Double.MIN_VALUE;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     private static final Scanner sc = new Scanner(System.in);
-    public static final String CONTINUE_OR_END = "Do you want to continue calculating? [yes/no]: ";
-    public static final String ENTER_EXPRESSION = "\nEnter a mathematical expression: ";
+    public static final String CONTINUATION_OR_END_MSG = "Do you want to continue calculating? [yes/no]: ";
+    public static final String INPUT_EXPRESSION_MSG = "\nEnter a mathematical expression: ";
     public static final String YES = "yes";
     public static final String NO = "no";
 
     public static void main(String[] args) {
         do {
-            System.out.print(ENTER_EXPRESSION);
+            System.out.print(INPUT_EXPRESSION_MSG);
             String expression = sc.nextLine();
             Calculator calc = new Calculator();
             double result = calc.calculate(expression);
@@ -21,9 +21,7 @@ public class CalculatorTest {
     }
 
     private static void printResult(double result, String expression) {
-        if (result == Double.MIN_VALUE) {
-            System.out.println();
-        } else {
+        if (result != Double.MIN_VALUE) {
             System.out.printf("%s = " + (result % 1 == 0 ? "%.0f\n" : "%.3f\n"), expression, result);
         }
     }
@@ -31,7 +29,7 @@ public class CalculatorTest {
     private static boolean isNext() {
         String answer;
         do {
-            System.out.print(CONTINUE_OR_END);
+            System.out.print(CONTINUATION_OR_END_MSG);
             answer = sc.next();
             sc.nextLine();
         } while (!answer.equals(YES) && !answer.equals(NO));
