560fc2dc2254fe16ef65ea76760ed855571eb7a2
Viktor Ivannikov <iceivannikov@gmail.com>
2023-10-15 17:22:50 +0300

    Completed the task Calculator *

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -11,27 +11,48 @@ public class Calculator {
     private static final String DELIMITER_REGEX = " ";
     private static final String DIVISION_BY_ZERO_EXCEPTION_MSG = "Exception!!! You can't divide by zero";
     private static final String NO_SIGN_MSG = "Error: sign [ %s ] is not supported\n";
+    public static final String INVALID_EXPRESSION_INPUT_MSG = "Invalid expression input, check the entered data";
+    public static final String NUMBERS_NOT_POSITIVE_MSG = "Numbers can only be positive";
+    public static final String EXPRESSION_MORE_23_CHARACTERS_MSG = "Expression length cannot exceed 23 characters";
+    public static final int MAX_EXPRESSION_LENGTH = 23;
 
-    public double calculate(String expression) {
-        String[] partsExpression = expression.split(DELIMITER_REGEX);
-        int a = Integer.parseInt(partsExpression[0]);
-        String sign = partsExpression[1];
-        int b = Integer.parseInt(partsExpression[2]);
-        return switch (sign) {
-            case PLUS -> a + b;
-            case MINUS -> a - b;
-            case MULTIPLY -> a * b;
-            case RAISE_TO_POWER -> Math.pow(a, b);
-            case DIVIDE -> b == 0 ? returnDoubleMinValue() : (double) a / b;
-            case REMAINDER_WITH_DIVISION -> b == 0 ? returnDoubleMinValue() : a % b;
-            default -> {
-                System.out.printf(NO_SIGN_MSG, sign);
-                yield Double.MIN_VALUE;
+    public static double calculate(String expression) {
+        int a;
+        String sign;
+        int b;
+        if (expression.length() <= MAX_EXPRESSION_LENGTH) {
+            String[] partsExpression = expression.split(DELIMITER_REGEX);
+            try {
+                a = Integer.parseInt(partsExpression[0]);
+                b = Integer.parseInt(partsExpression[2]);
+            } catch (RuntimeException exception) {
+                System.out.println(INVALID_EXPRESSION_INPUT_MSG);
+                return Double.MIN_VALUE;
             }
-        };
+            if (a < 0 || b < 0) {
+                System.out.println(NUMBERS_NOT_POSITIVE_MSG);
+                return Double.MIN_VALUE;
+            }
+            sign = partsExpression[1];
+            return switch (sign) {
+                case PLUS -> a + b;
+                case MINUS -> a - b;
+                case MULTIPLY -> a * b;
+                case RAISE_TO_POWER -> Math.pow(a, b);
+                case DIVIDE -> b == 0 ? returnDoubleMinValue() : (double) a / b;
+                case REMAINDER_WITH_DIVISION -> b == 0 ? returnDoubleMinValue() : a % b;
+                default -> {
+                    System.out.printf(NO_SIGN_MSG, sign);
+                    yield Double.MIN_VALUE;
+                }
+            };
+        } else {
+            System.out.println(EXPRESSION_MORE_23_CHARACTERS_MSG);
+        }
+        return Double.MIN_VALUE;
     }
 
-    private double returnDoubleMinValue() {
+    private static double returnDoubleMinValue() {
         print();
         return Double.MIN_VALUE;
     }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -7,18 +7,17 @@ public class CalculatorTest {
 
     private static final Scanner sc = new Scanner(System.in);
     public static final String CONTINUATION_OR_END_MSG = "Do you want to continue calculating? [yes/no]: ";
-    public static final String INPUT_EXPRESSION_MSG = "\nEnter a mathematical expression: ";
+    public static final String INPUT_EXPRESSION_MSG = "\nEnter a mathematical expression in the format [a + b]: ";
     public static final String YES = "yes";
     public static final String NO = "no";
-
+//23
     public static void main(String[] args) {
         String answer = YES;
         while (!Objects.equals(answer, NO)) {
             if (Objects.equals(answer, YES)) {
                 System.out.print(INPUT_EXPRESSION_MSG);
                 String expression = sc.nextLine();
-                Calculator calc = new Calculator();
-                double result = calc.calculate(expression);
+                double result = Calculator.calculate(expression);
                 printResult(result, expression);
             }
             System.out.print(CONTINUATION_OR_END_MSG);
