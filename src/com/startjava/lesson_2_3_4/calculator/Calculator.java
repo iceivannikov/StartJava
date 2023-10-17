1d3fc839e5a073c548ca318cf1d3566b5bc7b9d4
Viktor Ivannikov <iceivannikov@gmail.com>
2023-10-17 20:04:54 +0300

    Correct errors in the class Calculator lesson 4 task calculator *

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -8,60 +8,45 @@ public class Calculator {
     private static final String RAISE_TO_POWER = "^";
     private static final String DIVIDE = "/";
     private static final String REMAINDER_WITH_DIVISION = "%";
-    private static final String DELIMITER_REGEX = " ";
-    private static final String DIVISION_BY_ZERO_EXCEPTION_MSG = "Exception!!! You can't divide by zero";
-    private static final String NO_SIGN_MSG = "Error: sign [ %s ] is not supported\n";
-    public static final String INVALID_EXPRESSION_INPUT_MSG = "Invalid expression input, check the entered data";
-    public static final String NUMBERS_NOT_POSITIVE_MSG = "Numbers can only be positive";
-    public static final String EXPRESSION_LENGTH_LIMIT_MSG = "The length of the expression cannot exceed "
-            + Calculator.MAX_EXPRESSION_LENGTH + " characters and be less than " + Calculator.MIN_EXPRESSION_LENGTH;
-    public static final int MAX_EXPRESSION_LENGTH = 3;
-    public static final int MIN_EXPRESSION_LENGTH = 3;
+    private static final int MAX_EXPRESSION_LENGTH = 3;
 
-    public static double calculate(String expression) {
+    public static double calculate(String expression) throws RuntimeException {
         int a;
-        String sign;
         int b;
-        try {
-            if (expression.length() > MAX_EXPRESSION_LENGTH || expression.length() < MIN_EXPRESSION_LENGTH) {
-                throw new RuntimeException();
-            }
-            String[] partsExpression = expression.split(DELIMITER_REGEX);
-            a = Integer.parseInt(partsExpression[0]);
-            b = Integer.parseInt(partsExpression[2]);
-            if (a < 0 || b < 0) {
-                System.out.println(NUMBERS_NOT_POSITIVE_MSG);
-                return Double.MIN_VALUE;
-            }
-            sign = partsExpression[1];
-            return switch (sign) {
-                case PLUS -> a + b;
-                case MINUS -> a - b;
-                case MULTIPLY -> a * b;
-                case RAISE_TO_POWER -> Math.pow(a, b);
-                case DIVIDE -> b == 0 ? returnDoubleMinValue() : (double) a / b;
-                case REMAINDER_WITH_DIVISION -> a % b;
-                default -> {
-                    System.out.printf(NO_SIGN_MSG, sign);
-                    yield Double.MIN_VALUE;
-                }
-            };
-        } catch (NumberFormatException exception) {
-            System.out.println(INVALID_EXPRESSION_INPUT_MSG);
-        } catch (ArithmeticException exception) {
-            print();
-        } catch (RuntimeException exception) {
-            System.out.println(EXPRESSION_LENGTH_LIMIT_MSG);
+        String sign;
+        double result;
+        String[] partsExpression = expression.split(" ");
+        if (partsExpression.length != MAX_EXPRESSION_LENGTH) {
+            throw new IndexOutOfBoundsException();
         }
-        return Double.MIN_VALUE;
+        a = parseNumber(partsExpression[0]);
+        b = parseNumber(partsExpression[2]);
+        sign = partsExpression[1];
+        result = switch (sign) {
+            case PLUS -> a + b;
+            case MINUS -> a - b;
+            case MULTIPLY -> a * b;
+            case RAISE_TO_POWER -> Math.pow(a, b);
+            case DIVIDE -> div(b, a);
+            case REMAINDER_WITH_DIVISION -> a % b;
+            default -> throw new IllegalArgumentException();
+        };
+        return result;
     }
 
-    private static double returnDoubleMinValue() {
-        print();
-        return Double.MIN_VALUE;
+    private static double div(int b, double a) {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
     }
 
-    private static void print() {
-        System.out.println(DIVISION_BY_ZERO_EXCEPTION_MSG);
+    private static int parseNumber(String partExpression) {
+        int result;
+            result = Integer.parseInt(partExpression);
+        if (result < 0) {
+            throw new NumberFormatException();
+        }
+        return result;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,19 +6,36 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     private static final Scanner sc = new Scanner(System.in);
-    public static final String CONTINUATION_OR_END_MSG = "Do you want to continue calculating? [yes/no]: ";
-    public static final String INPUT_EXPRESSION_MSG = "\nEnter a mathematical expression in the format [a sign b]: ";
-    public static final String YES = "yes";
-    public static final String NO = "no";
+    private static final String CONTINUATION_OR_END_MSG = "Do you want to continue calculating? [yes/no]: ";
+    private static final String INPUT_EXPRESSION_MSG = "\nEnter a mathematical expression in the format [a sign b]: ";
+    private static final String YES = "yes";
+    private static final String NO = "no";
+    private static final String INVALID_EXPRESSION_INPUT_MSG = "The number entered must be positive and integer";
+    private static final String EXPRESSION_LENGTH_LIMIT_MSG = "The length of the expression cannot exceed "
+            + CalculatorTest.MAX_EXPRESSION_LENGTH + " characters and be less than "
+            + CalculatorTest.MAX_EXPRESSION_LENGTH;
+    private static final int MAX_EXPRESSION_LENGTH = 3;
+    private static final String DIVISION_BY_ZERO_EXCEPTION_MSG = "Exception!!! You can't divide by zero";
+    private static final String NO_SIGN_MSG = "Error: sign [ %s ] is not supported\n";
 
     public static void main(String[] args) {
         String answer = YES;
         while (!Objects.equals(answer, NO)) {
             if (Objects.equals(answer, YES)) {
-                System.out.print(INPUT_EXPRESSION_MSG);
-                String expression = sc.nextLine();
-                double result = Calculator.calculate(expression);
-                printResult(result, expression);
+                try {
+                    System.out.print(INPUT_EXPRESSION_MSG);
+                    String expression = sc.nextLine();
+                    double result = Calculator.calculate(expression);
+                    printResult(result, expression);
+                } catch (ArithmeticException exception) {
+                    System.out.println(DIVISION_BY_ZERO_EXCEPTION_MSG);
+                } catch (NumberFormatException exception) {
+                    System.out.println(INVALID_EXPRESSION_INPUT_MSG);
+                } catch (IndexOutOfBoundsException exception) {
+                    System.out.println(EXPRESSION_LENGTH_LIMIT_MSG);
+                } catch (IllegalArgumentException exception) {
+                    System.out.println(NO_SIGN_MSG);
+                }
             }
             System.out.print(CONTINUATION_OR_END_MSG);
             answer = sc.next();
@@ -27,8 +44,6 @@ public class CalculatorTest {
     }
 
     private static void printResult(double result, String expression) {
-        if (result != Double.MIN_VALUE) {
-            System.out.printf("%s = " + (result % 1 == 0 ? "%.0f\n" : "%.3f\n"), expression, result);
-        }
+        System.out.printf("%s = " + (result % 1 == 0 ? "%.0f\n" : "%.3f\n"), expression, result);
     }
 }
