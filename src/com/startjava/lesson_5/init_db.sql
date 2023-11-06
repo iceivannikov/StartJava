5e4a51385bef7cfdfd6bab5e70cf08bf016f85ce
Viktor Ivannikov <iceivannikov@gmail.com>
2023-11-06 11:36:28 +0300

    Final edits

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,12 +1,12 @@
-INSERT INTO jaegers(model_name, mark, height, weight, status, origin, launch, kaiju_kill) 
-VALUES('Brawler Yukon', 'Mark-1', 96.34, 3.46, 'Destroyed', 'USA', '2015-01-30', 2),
-      ('Cherno Alpha', 'Mark-1', 85.34, 2.412, 'Destroyed', 'Russia', '2015-10-25', 6),
-      ('Coyote Tango', 'Mark-1', 85.34, 2.312, 'Destroyed', 'Japan', '2015-12-30', 2),
-      ('Romeo Blue', 'Mark-1', 77.72, 7.775, 'Destroyed', 'USA', '2015-01-15', 2),
-      ('Horizon Brave', 'Mark-1', 72.54, 7.89, 'Destroyed', 'China', '2015-01-22', 2),
-      ('Diablo Intercept', 'Mark-2', 87.74, 6.234, 'Destroyed', 'China', '2016-06-03', 8),
-      ('Gipsy Danger', 'Mark-3', 79.25, 1.980, 'Destroyed', 'USA', '2017-06-10', 9),
-      ('Crimson Typhoon', 'Mark-4', 76.2, 1.722, 'Destroyed', 'China', '2018-08-22', 7),
-      ('Bracer Phoenix', 'Mark-5', 70.91, 2.128, 'Destroyed', 'USA', '2025-11-01', 5),
-      ('Titan Redeemer', 'Mark-6', 89.90, 3.245, 'Destroyed', 'Japan', '2029-03-01', 2),
-      ('Vanguard Horizon', 'Mark-6', 90.89, 4.678, 'Active', 'Russia', '2035-04-24', 1);
+INSERT INTO jaegers (model_name, mark, height, weight, status, origin, launch, kaiju_kill) 
+VALUES ('Brawler Yukon', 'Mark-1', 96.34, 3.46, 'Destroyed', 'USA', '2015-01-30', 2),
+       ('Cherno Alpha', 'Mark-1', 85.34, 2.412, 'Destroyed', 'Russia', '2015-10-25', 6),
+       ('Coyote Tango', 'Mark-1', 85.34, 2.312, 'Destroyed', 'Japan', '2015-12-30', 2),
+       ('Romeo Blue', 'Mark-1', 77.72, 7.775, 'Destroyed', 'USA', '2015-01-15', 2),
+       ('Horizon Brave', 'Mark-1', 72.54, 7.89, 'Destroyed', 'China', '2015-01-22', 2),
+       ('Diablo Intercept', 'Mark-2', 87.74, 6.234, 'Destroyed', 'China', '2016-06-03', 8),
+       ('Gipsy Danger', 'Mark-3', 79.25, 1.980, 'Destroyed', 'USA', '2017-06-10', 9),
+       ('Crimson Typhoon', 'Mark-4', 76.2, 1.722, 'Destroyed', 'China', '2018-08-22', 7),
+       ('Bracer Phoenix', 'Mark-5', 70.91, 2.128, 'Destroyed', 'USA', '2025-11-01', 5),
+       ('Titan Redeemer', 'Mark-6', 89.90, 3.245, 'Destroyed', 'Japan', '2029-03-01', 2),
+       ('Vanguard Horizon', 'Mark-6', 90.89, 4.678, 'Active', 'Russia', '2035-04-24', 1);
